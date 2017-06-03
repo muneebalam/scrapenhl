508962e51a9fdb70f9992d0eba3fbc9c581ed801
Muneeb Alam <muneeb.alam@gmail.com>
2017-06-03 15:28:37 -0400

    Started new package
    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'scrapenhl',
+      version = '0.1',
+      description = 'Scraping and manipulating NHL PbP data',
+      url = 'https://github.com/muneebalam/scrapenhl',
+      author = 'Muneeb Alam',
+      author_email = 'muneeb.alam@gmail.com',
+      license = 'MIT',
+      packages = ['scrapenhl'],
+      zip_safe = False)
