509a4fd13095caf88073a668ca975d95b74cdf84
Muneeb Alam <muneeb.alam@gmail.com>
2017-06-22 22:44:29 -0400

    Drafted update teamlogs working version

--- scrapenhl/scrape/scrape_season.py
+++ scrapenhl/scrape/scrape_season.py
@@ -74,9 +74,11 @@ def get_team_toilog_filename(season, team):
 
 def update_teamlogs(season, force_overwrite = False):
 
-    teams = set(
-        scrapenhl_globals.BASIC_GAMELOG.query('Season == {0:d}'.format(season))['Team'].value_counts().index())
-
+    teams = {x for x in \
+        scrapenhl_globals.BASIC_GAMELOG.query('Season == {0:d}'.format(season))['Home'].drop_duplicates()} | \
+            {x for x in \
+             scrapenhl_globals.BASIC_GAMELOG.query('Season == {0:d}'.format(season))['Away'].drop_duplicates()}
+    temp = scrapenhl_globals.BASIC_GAMELOG
     ### List files in correct format
     import os
     allfiles = os.listdir(scrapenhl_globals.get_season_folder(season))
@@ -84,71 +86,70 @@ def update_teamlogs(season, force_overwrite = False):
     pbpfiles = {int(x[:5]): x for x in allfiles if x[-12:] == '_parsed.zlib'}
     toifiles = {int(x[:5]): x for x in allfiles if x[-19:] == '_shifts_parsed.zlib'}
 
-    ### If force overwrite is false, read current log and see which games are already included
-    games_already_done = set()
-
     import feather
-    import zlib
+    import pandas as pd
+    import os.path
 
     for team in teams:
-        teamgames = scrapenhl_globals.BASIC_GAMELOG.query('Season == {0:d} & (Home == "{1:s} | Away == {1:s})'.format(
+        teamgames = scrapenhl_globals.BASIC_GAMELOG.query('Season == {0:d} & (Home == "{1:s}" | Away == "{1:s}")'.format(
             season, team))['Game']
-        try:
+        current_pbp = None
+        games_already_done = set()
+        if os.path.exists(get_team_pbplog_filename(season, team)):
             current_pbp = feather.read_dataframe(get_team_pbplog_filename(season, team))
             games_already_done = {x for x in current_pbp.Game}
-        except FileNotFoundError:
-            current_pbp = None
-            games_already_done = set()
 
         dflist = []
-        if not force_overwrite:
+        if not force_overwrite and current_pbp is not None:
             dflist.append(current_pbp)
             teamgames = {g for g in teamgames if g not in games_already_done}
-
-        if force_overwrite:
-            for game in teamgames:
-                r = open(scrape_game.get_parsed_save_filename(season, game), 'r')
-                page = r.read()
-                r.close()
-
-                df = zlib.decompress(zlib)
+        ### TODO do I need to flip any columns?
+        #if force_overwrite:
+        for game in teamgames:
+            try:
+                df = pd.read_hdf(scrape_game.get_parsed_save_filename(season, game))
                 df = df.assign(Game = game)
-                dflist.append(df)
+                if df is not None:
+                    dflist.append(df)
+            except FileNotFoundError:
+                pass
+        if len(dflist) > 0:
+            new_pbp = pd.concat(dflist)
+            for col in new_pbp.columns:
+                if new_pbp[col].dtype == 'object':
+                    new_pbp[col] = new_pbp[col].astype(str)
+            feather.write_dataframe(new_pbp, get_team_pbplog_filename(season, team))
 
-        import pandas as pd
-        new_pbp = pd.concat(dflist)
-        feather.write_dataframe(new_pbp, get_team_pbplog_filename(season, team))
-
-        try:
+        current_toi = None
+        games_already_done = set()
+        if os.path.exists(get_team_toilog_filename(season, team)):
             current_toi = feather.read_dataframe(get_team_toilog_filename(season, team))
             games_already_done = {x for x in current_toi.Game}
-        except FileNotFoundError:
-            current_toi = None
-            games_already_done = set()
-
+        ### TODO issues here
         dflist = []
         if not force_overwrite:
             dflist.append(current_toi)
             teamgames = {g for g in teamgames if g not in games_already_done}
 
-        if force_overwrite:
-            for game in teamgames:
-                r = open(scrape_game.get_parsed_shifts_save_filename(season, game), 'r')
-                page = r.read()
-                r.close()
-
-                df = zlib.decompress(zlib)
-                ### Columns for players are labeled [Team]1-6, [OppTeam]1-6, so changing opp team names to just 'Opp'
-                oppnamecolumns = [x for x in df.columns if x[:3] != team]
-                oppname = x[0][:3]
-                df.rename(columns={'{0:s}{1:d}'.format(oppname, i): 'Opp{0:d}'.format(i) for i in range(1, 7)},
-                          inplace=True)
+        #if force_overwrite:
+        for game in teamgames:
+            try:
+                df = pd.read_hdf(scrape_game.get_parsed_shifts_save_filename(season, game))
                 df = df.assign(Game = game)
-                dflist.append(df)
+                cols_to_replace = {col for col in df.columns if str.isdigit(col[-1]) if col[:3] != team}
+                df.rename(columns = {col: 'Opp' + col[-1] for col in cols_to_replace}, inplace = True)
+                if df is not None:
+                    dflist.append(df)
+            except FileNotFoundError:
+                pass
 
         import pandas as pd
-        new_toi = pd.concat(dflist)
-        feather.write_dataframe(new_toi, get_team_toilog_filename(season, team))
+        if len(dflist) > 0:
+            new_toi = pd.concat(dflist)
+            for col in new_toi.columns:
+                if new_toi[col].dtype == 'object':
+                    new_toi[col] = new_toi[col].astype(str)
+            feather.write_dataframe(new_toi, get_team_toilog_filename(season, team))
 
 def update_playerlog():
     pass
@@ -159,20 +160,20 @@ def get_season_schedule_url(season):
 
 def parse_games(season, games, force_overwrite = False, marker = 10):
     """
-        Parses the specified games.
+    Parses the specified games.
 
-        Parameters
-        -----------
-        season : int
-            The season of the game. 2007-08 would be 2007.
-        games : iterable of ints (e.g. list)
-            The game id. This can range from 20001 to 21230 for regular season, and 30111 to 30417 for playoffs.
-            The preseason, all-star game, Olympics, and World Cup also have game IDs that can be provided.
-        force_overwrite : bool
-            If True, will overwrite previously parsed files. If False, will not parise if files already found.
-        marker : float or int
-            The number of times to print progress. 10 will print every 10%; 20 every 5%.
-        """
+    Parameters
+    -----------
+    season : int
+        The season of the game. 2007-08 would be 2007.
+    games : iterable of ints (e.g. list)
+        The game id. This can range from 20001 to 21230 for regular season, and 30111 to 30417 for playoffs.
+        The preseason, all-star game, Olympics, and World Cup also have game IDs that can be provided.
+    force_overwrite : bool
+        If True, will overwrite previously parsed files. If False, will not parise if files already found.
+    marker : float or int
+        The number of times to print progress. 10 will print every 10%; 20 every 5%.
+    """
     games = sorted(list(games))
     marker_i = [len(games) // marker * i for i in range(marker)]
     marker_i[-1] = len(games) - 1
@@ -210,9 +211,18 @@ def autoupdate(season = scrapenhl_globals.MAX_SEASON):
             if game['status']['abstractGameState'] == 'Final':
                 completed_games.add(int(str(game['gamePk'])[-5:]))
 
-    #scrape_games(season, completed_games)
+    scrape_games(season, completed_games)
     parse_games(season, completed_games)
 
-for season in range(2012, 2017):
-    autoupdate(season)
+
+#update_teamlogs(2016)
+
+from urllib.error import URLError
+for season in range(2016, 2017):
+    while True:
+        try:
+            autoupdate(season)
+            break
+        except URLError as e:
+            print(season, e, e.args)
 #scrapenhl_globals.write_correct_playername_file()
