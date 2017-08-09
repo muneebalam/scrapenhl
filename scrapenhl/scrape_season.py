167f48c022b769339b40ecb4fd64c57b33e036d4
Muneeb Alam <muneeb.alam@gmail.com>
2017-08-08 21:24:10 -0400

    Moved 3 files

---
+++ scrapenhl/scrape_season.py
@@ -0,0 +1,343 @@
+import scrapenhl_globals
+import scrape_game
+
+def scrape_games(season, games, force_overwrite = False, pause = 1, marker = 10):
+    """
+    Scrapes the specified games.
+
+    Parameters
+    -----------
+    season : int
+        The season of the game. 2007-08 would be 2007.
+    games : iterable of ints (e.g. list)
+        The game id. This can range from 20001 to 21230 for regular season, and 30111 to 30417 for playoffs.
+        The preseason, all-star game, Olympics, and World Cup also have game IDs that can be provided.
+    force_overwrite : bool
+        If True, will overwrite previously raw html files. If False, will not scrape if files already found.
+    pause : float or int
+        The time to pause between requests to the NHL API. Defaults to 1 second
+    marker : float or int
+        The number of times to print progress. 10 will print every 10%; 20 every 5%.
+    """
+    import time
+    import datetime
+    starttime = time.time()
+    games = sorted(list(games))
+    marker_i = [len(games)//marker * i for i in range(marker)]
+    marker_i[-1] = len(games) - 1
+    marker_i_set = set(marker_i)
+    for i in range(len(games)):
+        game = games[i]
+        newscrape = scrape_game.scrape_game(season, game, force_overwrite)
+        if newscrape: #only sleep if had to scrape a new game
+            time.sleep(pause)
+        if i in marker_i_set:
+            print('Done through', season, game, ' ~ ', round((marker_i.index(i)) * 100/marker), '% in',
+                  str(datetime.timedelta(seconds = time.time() - starttime)))
+    print('Done scraping games in', season)
+
+
+def scrape_season(season, startgame = None, endgame = None, force_overwrite = False, pause = 1):
+    """
+    Scrapes games for the specified season.
+
+    Parameters
+    -----------
+    season : int
+        The season of the game. 2007-08 would be 2007.
+    startgame : int
+        The game id at which scraping will start. For example, midway through a season, this can be the last game
+        scraped.
+        This can range from 20001 to 21230 for regular season, and 30111 to 30417 for playoffs.
+        The preseason, all-star game, Olympics, and World Cup also have game IDs that can be provided.
+    force_overwrite : bool
+        If True, will overwrite previously raw html files. If False, will not scrape if files already found.
+    pause : float or int
+        The time to pause between requests to the NHL API. Defaults to 1 second
+    """
+    if season != 2012:
+        games = [20000 + x for x in range(1, 1231)]
+    else:
+        games = [20000 + x for x in range(1, 721)]
+    for round in range(1, 5):
+        for series in range(1, 8//round + 1):
+            for game in range(1, 8):
+                games.append(int('30{0:d}{1:d}{2:d}'.format(round, series, game)))
+    if startgame is not None:
+        games = [g for g in games if g >= startgame]
+    if endgame is not None:
+        games = [g for g in games if g <= endgame]
+    scrape_games(season, games, force_overwrite, pause, 10)
+
+def get_team_pbplog_filename(season, team):
+    return '{0:s}Team logs/{2:s}{1:d}_pbp.feather'.format(scrapenhl_globals.SAVE_FOLDER, season, team)
+
+def get_team_toilog_filename(season, team):
+    return '{0:s}Team logs/{2:s}{1:d}_toi.feather'.format(scrapenhl_globals.SAVE_FOLDER, season, team)
+
+def update_teamlogs(season, force_overwrite = False):
+    import os
+    import feather
+    import pandas as pd
+    import os.path
+
+    basic_gamelog = scrapenhl_globals.get_quick_gamelog_file()
+
+    teams = {x for x in \
+             basic_gamelog.query('Season == {0:d}'.format(season))['Home'].drop_duplicates()} | \
+            {x for x in \
+             basic_gamelog.query('Season == {0:d}'.format(season))['Away'].drop_duplicates()}
+    temp = basic_gamelog
+    ### List files in correct format
+    allfiles = os.listdir(scrapenhl_globals.get_season_folder(season))
+
+    pbpfiles = {int(x[:5]): x for x in allfiles if x[-12:] == '_parsed.zlib'}
+    toifiles = {int(x[:5]): x for x in allfiles if x[-19:] == '_shifts_parsed.zlib'}
+
+    for team in teams:
+        teamgames = {int(g) for g in basic_gamelog.query('Season == {0:d} & (Home == "{1:s}" | Away == "{1:s}")'.format(
+            season, team))['Game'].values}
+        current_pbp = None
+        games_already_done = set()
+        if os.path.exists(get_team_pbplog_filename(season, team)):
+            current_pbp = feather.read_dataframe(get_team_pbplog_filename(season, team))
+            games_already_done = {x for x in current_pbp.Game}
+
+        dflist = []
+        if not force_overwrite and current_pbp is not None:
+            dflist.append(current_pbp)
+            teamgames = {int(g) for g in teamgames if g not in games_already_done}
+        ### TODO do I need to flip any columns?
+        #if force_overwrite:
+        for game in teamgames:
+            try:
+                df = pd.read_hdf(scrape_game.get_parsed_save_filename(season, game))
+                df = df.assign(Game = game)
+                if df is not None:
+                    dflist.append(df)
+            except FileNotFoundError:
+                pass
+        if len(dflist) > 0:
+            new_pbp = pd.concat(dflist)
+            for col in new_pbp.columns:
+                if new_pbp[col].dtype == 'object':
+                    new_pbp[col] = new_pbp[col].astype(str)
+            feather.write_dataframe(new_pbp, get_team_pbplog_filename(season, team))
+
+        current_toi = None
+        games_already_done = set()
+        if os.path.exists(get_team_toilog_filename(season, team)):
+            current_toi = feather.read_dataframe(get_team_toilog_filename(season, team))
+            games_already_done = {x for x in current_toi.Game}
+        ### TODO issues here
+        dflist = []
+        if not force_overwrite:
+            dflist.append(current_toi)
+            teamgames = {g for g in teamgames if g not in games_already_done}
+
+        #if force_overwrite:
+        for game in teamgames:
+            try:
+                df = pd.read_hdf(scrape_game.get_parsed_shifts_save_filename(season, game))
+                df = df.assign(Game = game)
+                cols_to_replace = {col for col in df.columns if str.isdigit(col[-1]) if col[:3] != team}
+                df.rename(columns = {col: 'Opp' + col[3:] for col in cols_to_replace}, inplace = True)
+                if df is not None:
+                    dflist.append(df)
+            except FileNotFoundError:
+                pass
+
+        import pandas as pd
+        dflist = [df for df in dflist if df is not None]
+        if len(dflist) > 0:
+            new_toi = pd.concat(dflist)
+            for col in new_toi.columns:
+                if new_toi[col].dtype == 'object':
+                    new_toi[col] = new_toi[col].astype(str)
+            feather.write_dataframe(new_toi, get_team_toilog_filename(season, team))
+
+def get_team_toilog(season, team):
+    import feather
+    return feather.read_dataframe(get_team_toilog_filename(season, team))
+
+def get_team_pbplog(season, team):
+    import feather
+    return feather.read_dataframe(get_team_pbplog_filename(season, team))
+
+def get_season_schedule_url(season):
+    return 'https://statsapi.web.nhl.com/api/v1/schedule?startDate={0:d}-09-01&endDate={1:d}-06-25'.format(season,
+                                                                                                           season + 1)
+
+def parse_games(season, games, force_overwrite = False, marker = 10):
+    """
+    Parses the specified games.
+
+    Parameters
+    -----------
+    season : int
+        The season of the game. 2007-08 would be 2007.
+    games : iterable of ints (e.g. list)
+        The game id. This can range from 20001 to 21230 for regular season, and 30111 to 30417 for playoffs.
+        The preseason, all-star game, Olympics, and World Cup also have game IDs that can be provided.
+    force_overwrite : bool
+        If True, will overwrite previously parsed files. If False, will not parise if files already found.
+    marker : float or int
+        The number of times to print progress. 10 will print every 10%; 20 every 5%.
+    """
+    import time
+    import datetime
+
+    starttime = time.time()
+
+    games = sorted(list(games))
+    marker_i = [len(games) // marker * i for i in range(marker)]
+    marker_i[-1] = len(games) - 1
+    marker_i_set = set(marker_i)
+    for i in range(len(games)):
+        game = games[i]
+        scrape_game.parse_game(season, game, force_overwrite)
+        if i in marker_i_set:
+            print('Done through', season, game, ' ~ ', round((marker_i.index(i)) * 100 / marker), '% in',
+                  str(datetime.timedelta(seconds=time.time() - starttime)))
+    print('Done parsing games in', season)
+
+def autoupdate(season = scrapenhl_globals.MAX_SEASON):
+    """
+    Scrapes unscraped games for the specified season.
+
+    This is a convenience function that finds the highest completed game in a year and scrapes up to that point only.
+    This reduces unnecessary requests for unplayed games.
+
+    Parameters
+    -----------
+    season : int
+        The season of the game. 2007-08 would be 2007.
+    """
+    import urllib.request
+    url = get_season_schedule_url(season)
+    with urllib.request.urlopen(url) as reader:
+        page = reader.read().decode('latin-1')
+
+    import json
+    jsonpage = json.loads(page)
+    completed_games = set()
+
+    for gameday in jsonpage['dates']:
+        for game in gameday['games']:
+            if game['status']['abstractGameState'] == 'Final':
+                completed_games.add(int(str(game['gamePk'])[-5:]))
+
+    scrape_games(season, completed_games)
+    parse_games(season, completed_games)
+
+def read_completed_games_from_url(season):
+    import urllib.request
+    url = get_season_schedule_url(season)
+    with urllib.request.urlopen(url) as reader:
+        page = reader.read().decode('latin-1')
+
+    import json
+    jsonpage = json.loads(page)
+    completed_games = set()
+
+    for gameday in jsonpage['dates']:
+        for game in gameday['games']:
+            if game['status']['abstractGameState'] == 'Final':
+                completed_games.add(int(str(game['gamePk'])[-5:]))
+    return completed_games
+
+def reparse_season(season = scrapenhl_globals.MAX_SEASON):
+    """
+    Re-parses entire season.
+    :param season: int
+        The season of the game. 2007-08 would be 2007.
+    :return:
+    """
+    completed_games = read_completed_games_from_url(season)
+    parse_games(season, completed_games, True)
+
+
+def rewrite_globals(start_from_scratch = True, seasons = None):
+    """
+    Recreates global files: PLAYER_IDS, BASIC_GAMELOG, TEAM_IDS, CORRECTED_PLAYERNAMES
+
+    Parameters
+    -----------
+    seasons : list of int or None
+        The seasons of the games. 2007-08 would be 2007. Should only be provided when start_from_scratch is False.
+    start_from_scratch: bool
+        If True, will search through all files; if False, will look only at missing games in BASIC_GAMELOG.
+        False not yet implemented.
+    """
+
+    import os.path
+    import zlib
+    import json
+    import pandas as pd
+    import time
+    import datetime
+
+    if seasons is None:
+        seasons = [i for i in range(2007, scrapenhl_globals.MAX_SEASON + 1)]
+    elif isinstance(seasons, int):
+        seasons = [seasons]
+
+    if start_from_scratch:
+        import os
+        try:
+            os.remove(scrapenhl_globals.PLAYER_ID_FILE)
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove(scrapenhl_globals.TEAM_ID_FILE)
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove(scrapenhl_globals.BASIC_GAMELOG_FILE)
+        except FileNotFoundError:
+            pass
+
+    for season in seasons:
+
+        starttime = time.time()
+
+        games = read_completed_games_from_url(season)
+
+        marker = 20
+        games = sorted(list(games))
+        marker_i = [len(games) // marker * i for i in range(marker)]
+        marker_i[-1] = len(games) - 1
+        marker_i_set = set(marker_i)
+
+        for i in range(len(games)):
+            game = games[i]
+            #print(season, game)
+            filename = scrape_game.get_parsed_save_filename(season, game)
+            if os.path.exists(scrape_game.get_json_save_filename(season, game)):
+                r = open(scrape_game.get_json_save_filename(season, game), 'rb')
+                page = r.read()
+                r.close()
+
+                page = zlib.decompress(page)
+                try:
+                    data = json.loads(page.decode('latin-1'))
+
+                    teamdata = data['liveData']['boxscore']['teams']
+
+                    scrape_game.update_team_ids_from_json(teamdata)
+                    scrape_game.update_player_ids_from_json(teamdata)
+                    scrape_game.update_quick_gamelog_from_json(data)
+                except json.JSONDecodeError:
+                    pass
+
+            if i in marker_i_set:
+                print('Done through', season, game, ' ~ ', round((marker_i.index(i)) * 100 / marker), '% in ',
+                      str(datetime.timedelta(seconds = time.time() - starttime)))
+
+        print('Done with', season)
+
+if __name__ == "__main__":
+    #for season in range(2007, 2017):
+    #    autoupdate(season)
+    #    update_teamlogs(season, True)
+    pass
---
+++ scrapenhl/scrapenhl_globals.py
@@ -0,0 +1,237 @@
+"""
+File and folder paths, and other variables needed by all modules in this package.
+"""
+
+SAVE_FOLDER = "./data/"
+PLAYER_ID_FILE = "{0:s}reference/playerids.feather".format(SAVE_FOLDER)
+TEAM_ID_FILE = "{0:s}reference/teamids.feather".format(SAVE_FOLDER)
+BASIC_GAMELOG_FILE = "{0:s}reference/quickgamelog.feather".format(SAVE_FOLDER)
+PLAYER_NAMES_FILE = "{0:s}reference/playerids_names.feather".format(SAVE_FOLDER)
+
+import datetime
+MAX_SEASON = datetime.datetime.now().year - 1
+if datetime.datetime.now().month >= 9:
+    MAX_SEASON += 1
+import feather
+import pandas as pd
+import os.path
+
+def create_season_folder(season):
+    """
+    Creates a season folder in SAVE_FOLDER
+
+    Parameters
+    -----------
+    season : int
+        The season of the game. 2007-08 would be 2007.
+    """
+    import os
+    folder = '{0:s}{1:d}/'.format(SAVE_FOLDER, season)
+    os.mkdir(folder)
+
+def get_season_folder(season):
+    """
+    Returns season folder in SAVE_FOLDER
+
+    Parameters
+    -----------
+    season : int
+        The season of the game. 2007-08 would be 2007.
+
+    Returns
+    -------
+    str
+        The folder path
+    """
+    return '{0:s}{1:d}/'.format(SAVE_FOLDER, season)
+
+def get_player_id_file():
+    """
+    Returns the player id file
+
+    This file maps player IDs to names, positions, handedness, teams, and jersey numbers. Using IDs is a way to avoid
+    having to correct the numerous spelling inconsistencies in the data.
+
+    If the player ID file is not found, a blank one is created
+
+    Returns
+    --------
+    Pandas df
+        The player id dataframe
+    """
+    if not os.path.exists(PLAYER_ID_FILE):
+        print('Creating blank player ID file for future use')
+        df = pd.DataFrame({'ID': [], 'Name': [], 'Team': [], 'Pos': [], '#': [], 'Hand': [], 'Count': []})
+        #write_player_id_file()
+        return df
+    else:
+        return feather.read_dataframe(PLAYER_ID_FILE)
+
+def write_player_id_file(df):
+    """
+    Writes the player id file to disk in feather format
+
+    This file maps player IDs to names, positions, handedness, teams, and jersey numbers. Using IDs is a way to avoid
+    having to correct the numerous spelling inconsistencies in the data.
+    """
+    df.sort_values(by = "ID", inplace = True)
+    df['#'] = df['#'].astype(int)
+    df['ID'] = df['ID'].astype(str)
+    df['Name'] = df['Name'].astype(str)
+    df['Pos'] = df['Pos'].astype(str)
+    df['Team'] = df['Team'].astype(str)
+    df['Hand'] = df['Hand'].astype(str)
+    df['Count'] = df['Count'].astype(int)
+    feather.write_dataframe(df, PLAYER_ID_FILE)
+
+def get_team_id_file():
+    """
+    Returns the team id file
+
+    This file maps team IDs to names and abbreviations.
+
+    If the team ID file is not found, a blank one is created
+
+    Returns
+    --------
+    Pandas df
+        The team id dataframe
+    """
+    if not os.path.exists(TEAM_ID_FILE):
+        print('Creating blank team ID file for future use')
+        TEAM_IDS = pd.DataFrame({'ID': [], 'Name': [], 'Abbreviation': []})
+        # write_player_id_file()
+        return TEAM_IDS
+    else:
+        return feather.read_dataframe(TEAM_ID_FILE)
+
+def write_team_id_file(df):
+    """
+    Writes the team id file to disk in feather format
+
+    This file maps team IDs to names and abbreviations.
+    """
+    df.sort_values(by="ID", inplace=True)
+    feather.write_dataframe(df, TEAM_ID_FILE)
+
+def get_quick_gamelog_file():
+    """
+    Returns the game log file
+
+    This file contains basic information on games: season, game, date, venue, and home and road names, scores, and
+    coaches. A new blank file is created if necessary.
+
+    Returns
+    --------
+    Pandas df
+        The game log dataframe
+    """
+    if not os.path.exists(BASIC_GAMELOG_FILE):
+        print('Creating blank game log file for future use')
+        df = pd.DataFrame({'Season': [], 'Game': [], 'Datetime': [], 'Venue': [],
+                           'Home': [], 'HomeCoach': [], 'HomeScore': [],
+                           'Away': [], 'AwayCoach': [], 'AwayScore': []})
+        return df
+    else:
+        return feather.read_dataframe(BASIC_GAMELOG_FILE)
+
+def write_quick_gamelog_file(df):
+    """
+    Writes the game log dataframe (in global namespace) to disk in feather format
+    """
+    df.sort_values(by = ['Season', 'Game'], inplace = True)
+    df = df.drop_duplicates()
+    feather.write_dataframe(df, BASIC_GAMELOG_FILE)
+
+def write_preferred_player_names_file():
+    """
+    Uses player_ids file, assigns most common spelling of name to each ID
+    """
+    df = get_player_id_file()
+    highn = df[['ID', 'Name', 'Count']].groupby(['ID', 'Name']).sum() \
+        .reset_index() \
+        .sort_values(by='Count', ascending=False) \
+        .groupby('ID').iloc[0].reset_index()
+    feather.write_dataframe(highn, PLAYER_NAMES_FILE)
+
+def get_preferred_player_names():
+    try:
+        return feather.read_dataframe(PLAYER_NAMES_FILE)
+    except Exception as e:
+        write_preferred_player_names_file()
+        return feather.read_dataframe(PLAYER_NAMES_FILE)
+
+def player_name_to_id(pname, team_helper=None):
+    """
+    Matches given player name to ID. If multiple matches, prints warning and continues with most common player.
+
+    This method first looks for exact matches. If none are found, it looks for names containing given name.
+    If there are still no matches, it prints a warning and takes the closest fuzzy match.
+
+    Parameters
+    -----------
+    pname : str
+        The player name. Case sensitive.
+    team_helper: iterable of str, or str, or None
+        Can be used to help break exact matches
+
+    Returns
+    --------
+    id: int
+        The ID number of the player
+    """
+    df = get_player_id_file()
+    df = df[['Name', 'ID', 'Team', 'Count']].groupby(['Name', 'ID', 'Team']).sum() \
+        .reset_index().sort_values('Count', ascending=False)
+
+    if team_helper is not None:
+        if isinstance(team_helper, str):
+            team_helper = {team_helper}
+
+    ### Exact match
+    exact = df[df.Name == pname]
+    if len(exact.ID.unique()) == 1:
+        return exact.ID.iloc[0]
+    elif len(exact.ID.unique()) > 1:
+        if team_helper is None:
+            print('Found multiple matches for', pname)
+            print(exact)
+            print('Selecting', exact.iloc[0, :])
+            return exact.ID.iloc[0]
+        else:
+            exact_team = exact[exact.Team.isin(team_helper)]
+            if len(exact_team) == 1:
+                return exact_team.ID.iloc[0]
+            elif len(exact_team) > 1:
+                print('Found multiple matches for', pname)
+                print(exact_team)
+                print('Selecting', exact_team.iloc[0,:])
+                return exact_team.ID.iloc[0]
+
+    ### Contains match
+    contains = df[df.Name.str.contains(pname)]
+    if len(contains.ID.unique()) == 1:
+        return contains.ID.iloc[0]
+    elif len(contains.ID.unique()) > 1:
+        if team_helper is None:
+            print('Found multiple matches for', pname)
+            print(contains)
+            print('Selecting', contains.iloc[0, :])
+            return contains.ID.iloc[0]
+        else:
+            contains_team = contains[contains.Team.isin(team_helper)]
+            if len(contains_team) == 1:
+                return contains_team.ID.iloc[0]
+            elif len(contains_team) > 1:
+                print('Found multiple matches for', pname)
+                print(contains_team)
+                print('Selecting', contains_team.iloc[0,:])
+                return contains_team['ID'].iloc[0]
+
+    ### Fuzzy match
+    ### TODO using fuzzywuzzy
+    print('Match not found. Fuzzy matching to be implemented later')
+    return None
+
+
+
